62310be73934100adc63f8cdbe406425c57bd4ae
Guillaume <guillaume.p.chanut@gmail.com>
2020-07-11 03:02:51 +0200

    1st try refactoring service ctrl

--- WinServicesNotify/Form1.cs
+++ WinServicesNotify/Form1.cs
@@ -34,8 +34,6 @@ namespace WinServicesNotify
         /// <summary>
         /// Tricks to ensure to "minimized" the window in the icon tray not the taskbar
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void ServicesNotify_Move(object sender, EventArgs e)
         {
             if (this.WindowState == FormWindowState.Minimized)
@@ -44,24 +42,14 @@ namespace WinServicesNotify
             }
         }
 
-        //private void menuShow(object sender, EventArgs e)
-        //{
-        //    servicesMenuStrip.Show();
-        //}
-
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
 
-        //private void BalloonTest(object sender, EventArgs e)
-        //{
-        //    notifyIcon1.ShowBalloonTip(42000, "Test", "ouais", ToolTipIcon.Info);
-        //}
-
         /// <summary>
         /// Append the list of services in the ToolStripMenuItem 
-        /// with the right control (start/stop/restart) for each one ans a icon indicator of current status
+        /// with the right control (start/stop/restart) for each one and an icon indicator of current status
         /// </summary>
         private void listServices()
         {
@@ -72,138 +60,9 @@ namespace WinServicesNotify
 
             foreach (ServiceController scTemp in scServices)
             {
-                ToolStripMenuItem tmpTSMI = new System.Windows.Forms.ToolStripMenuItem();
-
-                tmpTSMI.Text = scTemp.ServiceName.ToString();
-                tmpTSMI.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { });
-
-                if (scTemp.Status.Equals(ServiceControllerStatus.Stopped))
-                {
-                    ToolStripItem startItem = new MyServiceCtrlItem(scTemp);
-
-                    startItem.Text = "Start";
-                    startItem.Click += new System.EventHandler(this.scStarter);
-
-                    tmpTSMI.Image = global::WinServicesNotify.Properties.Resources.rdRed;
-                    tmpTSMI.DropDownItems.Add(startItem);
-                }
-                if (scTemp.Status.Equals(ServiceControllerStatus.Running))
-                {
-                    ToolStripItem stopItem = new MyServiceCtrlItem(scTemp);
-                    ToolStripItem restartItem = new MyServiceCtrlItem(scTemp);
-
-                    tmpTSMI.Image = global::WinServicesNotify.Properties.Resources.rdGreen;
-
-                    if (scTemp.CanStop)
-                    {
-                        stopItem.Text = "Stop";
-                        stopItem.Click += new System.EventHandler(this.scStoper);
-
-                        restartItem.Text = "Restart";
-                        restartItem.Click += new System.EventHandler(this.scRestarter);
-
-                    }
-                    else
-                    {
-                        tmpTSMI.ForeColor = System.Drawing.SystemColors.GrayText;
-
-                        stopItem.Text = "Can't Stop";
-                        stopItem.Image = global::WinServicesNotify.Properties.Resources._lock;
-                        stopItem.ForeColor = System.Drawing.SystemColors.GrayText;
-
-                        restartItem.Text = "Can't restart";
-                        restartItem.Image = global::WinServicesNotify.Properties.Resources._lock;
-                        restartItem.ForeColor = System.Drawing.SystemColors.GrayText;
-                    }
-
-                    tmpTSMI.DropDownItems.Add(stopItem);
-                    tmpTSMI.DropDownItems.Add(restartItem);
-                }
+                ToolStripMenuItem tmpTSMI = new MyServiceMenuItem(scTemp);
 
                 this.servicesMenuStrip.Items.Add(tmpTSMI);
-            }
-        }
-
-        /// <summary>
-        /// Handle the starting of the passed service and notify it via Balloon Tip
-        /// </summary>
-        /// <param name="sender">ServiceController</param>
-        /// <param name="e"></param>
-        private void scStarter(object sender, EventArgs e)
-        {
-            MyServiceCtrlItem msci = sender as MyServiceCtrlItem;
-
-            try
-            {
-                msci.Sc.Start();
-                msci.Sc.WaitForStatus(ServiceControllerStatus.Running);
-                notifyIcon1.ShowBalloonTip(4200, "Service started", msci.Sc.DisplayName.ToString() + " started", ToolTipIcon.Info);
-            }
-            catch (Exception ex)
-            {
-                notifyIcon1.ShowBalloonTip(4200, "Service error", msci.Sc.DisplayName.ToString() + " can't start", ToolTipIcon.Error);
-
-                throw ex;
-            }
-        }
-
-        /// <summary>
-        /// Handle the stopping of the passed service and notify it via Balloon Tip
-        /// </summary>
-        /// <param name="sender">ServiceController</param>
-        /// <param name="e"></param>
-        private void scStoper(object sender, EventArgs e)
-        {
-            MyServiceCtrlItem msci = sender as MyServiceCtrlItem;
-
-            try
-            {
-                msci.Sc.Stop();
-                msci.Sc.WaitForStatus(ServiceControllerStatus.Stopped);
-                notifyIcon1.ShowBalloonTip(4200, "Service stopped", msci.Sc.DisplayName.ToString() + " stopped", ToolTipIcon.Info);
-            }
-            catch (Exception ex)
-            {
-                notifyIcon1.ShowBalloonTip(4200, "Service error", msci.Sc.DisplayName.ToString() + " can't stop", ToolTipIcon.Error);
-
-                throw ex;
-            }
-        }
-
-        /// <summary>
-        /// Handle the restarting of the passed service and notify it via Balloon Tip
-        /// </summary>
-        /// <param name="sender">ServiceController</param>
-        /// <param name="e"></param>
-        private void scRestarter(object sender, EventArgs e)
-        {
-
-            MyServiceCtrlItem msci = sender as MyServiceCtrlItem;
-
-            try
-            {
-                msci.Sc.Stop();
-                notifyIcon1.ShowBalloonTip(4200, "Service stopped", msci.Sc.ServiceName.ToString() + " stopped", ToolTipIcon.Info);
-            }
-            catch (Exception ex)
-            {
-                notifyIcon1.ShowBalloonTip(4200, "Service error", msci.Sc.DisplayName.ToString() + " can't stop", ToolTipIcon.Error);
-
-                throw ex;
-            }
-
-            msci.Sc.WaitForStatus(ServiceControllerStatus.Stopped);
-
-            try
-            {
-                msci.Sc.Start();
-                notifyIcon1.ShowBalloonTip(4200, "Service started", msci.Sc.DisplayName.ToString() + " restarted", ToolTipIcon.Info);
-            }
-            catch (Exception ex)
-            {
-                notifyIcon1.ShowBalloonTip(4200, "Service error", msci.Sc.DisplayName.ToString() + " can't start", ToolTipIcon.Error);
-
-                throw ex;
             }
         }
 
---
+++ WinServicesNotify/MyServiceMenuItem.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+using System.ServiceProcess;
+
+namespace WinServicesNotify
+{
+    public partial class MyServiceMenuItem : ToolStripMenuItem
+    {
+        private ServiceController _sc;
+
+        public ServiceController Sc
+        {
+            get { return _sc; }
+            set { _sc = value; }
+        }
+
+        public MyServiceMenuItem()
+        {
+            InitializeComponent();
+        }
+
+        public MyServiceMenuItem(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        public MyServiceMenuItem(ServiceController sc)
+        {
+            InitializeComponent();
+
+            this.Sc = sc;
+            this.Text = this.Sc.ServiceName.ToString();
+            this.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { });
+        }
+
+        private void buildMenu()
+        {
+            if (this.Sc.Status.Equals(ServiceControllerStatus.Stopped))
+            {
+                ToolStripItem startItem = new MyServiceCtrlItem(this.Sc);
+
+                startItem.Text = "Start";
+                startItem.Click += new System.EventHandler(this.ServiceCtrlHandlerStarter);
+
+                this.Image = global::WinServicesNotify.Properties.Resources.rdRed;
+                this.DropDownItems.Add(startItem);
+            }
+            if (this.Sc.Status.Equals(ServiceControllerStatus.Running))
+            {
+                ToolStripItem stopItem = new MyServiceCtrlItem(this.Sc);
+                ToolStripItem restartItem = new MyServiceCtrlItem(this.Sc);
+
+                this.Image = global::WinServicesNotify.Properties.Resources.rdGreen;
+
+                if (this.Sc.CanStop)
+                {
+                    stopItem.Text = "Stop";
+                    stopItem.Click += new System.EventHandler(this.ServiceCtrlHandlerStopper);
+
+                    restartItem.Text = "Restart";
+                    restartItem.Click += new System.EventHandler(this.ServiceCtrlHandlerRestart);
+
+                }
+                else
+                {
+                    this.ForeColor = System.Drawing.SystemColors.GrayText;
+
+                    stopItem.Text = "Can't Stop";
+                    stopItem.Image = global::WinServicesNotify.Properties.Resources._lock;
+                    stopItem.ForeColor = System.Drawing.SystemColors.GrayText;
+
+                    restartItem.Text = "Can't restart";
+                    restartItem.Image = global::WinServicesNotify.Properties.Resources._lock;
+                    restartItem.ForeColor = System.Drawing.SystemColors.GrayText;
+                }
+
+                this.DropDownItems.Add(stopItem);
+                this.DropDownItems.Add(restartItem);
+            }
+        }
+
+        /// <summary>
+        /// Handle the starting of the passed service and notify it via Balloon Tip
+        /// </summary>
+        /// <param name="sender">ServiceController</param>
+        /// <param name="e"></param>
+        protected void ServiceCtrlHandlerStarter(object sender, EventArgs e)
+        {
+            MyServiceCtrlItem msci = sender as MyServiceCtrlItem;
+            ToolStripMenuItem tsmi = sender as ToolStripMenuItem;
+
+            this.ServiceCtrlStarter(msci.Sc);
+            tsmi.Image = global::WinServicesNotify.Properties.Resources.rdGreen;
+        }
+
+        /// <summary>
+        /// Handle the stopping of the passed service and notify it via Balloon Tip
+        /// </summary>
+        /// <param name="sender">ServiceController</param>
+        /// <param name="e"></param>
+        protected void ServiceCtrlHandlerStopper(object sender, EventArgs e)
+        {
+            MyServiceCtrlItem msci = sender as MyServiceCtrlItem;
+
+            this.ServiceCtrlStopper(msci.Sc);
+        }
+
+        /// <summary>
+        /// Handle the restarting of the passed service and notify it via Balloon Tip
+        /// </summary>
+        /// <param name="sender">ServiceController</param>
+        /// <param name="e"></param>
+        protected void ServiceCtrlHandlerRestart(object sender, EventArgs e)
+        {
+            MyServiceCtrlItem msci = sender as MyServiceCtrlItem;
+
+            this.ServiceCtrlStopper(msci.Sc);
+            this.ServiceCtrlStarter(msci.Sc);
+        }
+
+        // Some defines for Balloons natifications
+        public const int TOV = 42000;
+        public const String ALRTUP = "Service started";
+        public const String ALRTDW = "Service stopped";
+        public const String ALRTERR = "Service error";
+
+        //
+        //
+        // COMMENT REVENIR SUR LE PARENT ?
+        //
+        //
+
+        private void ServiceCtrlStarter(ServiceController sc)
+        {
+            try
+            {
+                sc.Start();
+                sc.WaitForStatus(ServiceControllerStatus.Running);
+                //notifyIcon1.ShowBalloonTip(ServicesNotify.TOV,
+                //    ServicesNotify.ALRTUP,
+                //    sc.DisplayName.ToString() + " now " + sc.Status.ToString().ToLower(),
+                //    ToolTipIcon.Info);
+            }
+            catch (Exception ex)
+            {
+                //notifyIcon1.ShowBalloonTip(ServicesNotify.TOV,
+                //    ServicesNotify.ALRTERR,
+                //    sc.DisplayName.ToString() + " can't start",
+                //    ToolTipIcon.Error);
+
+                throw ex;
+            }
+        }
+        private void ServiceCtrlStopper(ServiceController sc)
+        {
+            try
+            {
+                sc.Stop();
+                sc.WaitForStatus(ServiceControllerStatus.Stopped);
+                notifyIcon1.ShowBalloonTip(ServicesNotify.TOV,
+                    ServicesNotify.ALRTDW,
+                    sc.ServiceName.ToString() + " now " + sc.Status.ToString().ToLower(),
+                    ToolTipIcon.Info);
+            }
+            catch (Exception ex)
+            {
+                notifyIcon1.ShowBalloonTip(ServicesNotify.TOV,
+                    ServicesNotify.ALRTERR,
+                    sc.DisplayName.ToString() + " can't stop",
+                    ToolTipIcon.Error);
+
+                throw ex;
+            }
+        }
+        private void ServiceCtrlInvoker(MyServiceCtrlItem msci, ServiceController sc)
+        {
+            // TODO
+        }
+    }
+}
